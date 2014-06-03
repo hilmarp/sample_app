c457f8c348e024979cad0c967666fd2fb93f48e6
Hilmar <hilmar@eht.is>
2014-06-03 21:20:00 +0000

    Finish user edit, update, index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :user do
-		name "Hilmar Pálsson"
-		email "hilmar@eht.is"
-		password "hilmar"
-		password_confirmation "hilmar"
-	end
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
 end
